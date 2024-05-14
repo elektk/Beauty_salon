b4465d57213778d61e5b6ae83e005285a86a8fd5
tka4 <suportenik@gmail.com>
2024-05-14 16:35:59 +0300

    update

--- src/pages/price-page/price-page.jsx
+++ src/pages/price-page/price-page.jsx
@@ -2,53 +2,59 @@ import styles from '../../css/price-page.module.css';
 import { useState, useEffect } from 'react';
 
 export default function Price() {
-    const [priceList, setPriceList] = useState([]);
+  const [priceList, setPriceList] = useState([]);
 
-    useEffect(() => {
-        fetch('http://localhost:8000/price/get-list')
-            .then((res) => res.json())
-            .then((data) => setPriceList(data));
-    }, []);
+  useEffect(() => {
+    fetch('http://localhost:8000/price/get-list')
+      .then((res) => res.json())
+      .then((data) => setPriceList(data));
+  }, []);
 
-    const renderPrice = (price) => (
-        <div className={styles.price} key={price.id}>
-            <div className={styles.text}>
-                <p>{price.title}</p>
-                <button>+ обработка кончинков волос</button>
-            </div>
-            <p className={styles.value}>{price.price} ₽</p>
+  const renderPrice = (price) => (
+    <div className={styles.price} key={price.id}>
+      <div className={styles.text}>
+        <p>{price.title}</p>
+        <button>+ обработка кончинков волос</button>
+      </div>
+      <p className={styles.value}>{price.price} ₽</p>
+    </div>
+  );
+
+  return (
+    <>
+      <div className={styles.title}>
+        <h2>Цены на услуги</h2>
+      </div>
+      <div className={styles.services}>
+        <div className={styles.content}>
+          <div className={styles.master} />
+          <div className={styles.righhtBlock}>
+            {priceList.slice(0, 4).map((price, index) => (
+              <div key={index}>{renderPrice(price)}</div>
+            ))}
+          </div>
         </div>
-    );
-
-    return (
-        <>
-            <div className={styles.title}>
-                <h2>Цены на услуги</h2>
-            </div>
-            <div className={styles.services}>
-                <div className={styles.content}>
-                    <div className={styles.master} />
-                    <div className={styles.righhtBlock}>
-                        {priceList.slice(0, 4).map(renderPrice)}
-                    </div>
-                </div>
-            </div>
-            <div className={styles.services}>
-                <div className={styles.content}>
-                    <div className={styles.reverse}>
-                        {priceList.slice(4, 8).map(renderPrice)}
-                    </div>
-                    <div className={styles.master} />
-                </div>
-            </div>
-            <div className={styles.services}>
-                <div className={styles.content}>
-                    <div className={styles.master} />
-                    <div className={styles.righhtBlock}>
-                        {priceList.slice(8, 12).map(renderPrice)}
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+      </div>
+      <div className={styles.services}>
+        <div className={styles.content}>
+          <div className={styles.reverse}>
+            {priceList.slice(4, 8).map((price, index) => (
+              <div key={index}>{renderPrice(price)}</div>
+            ))}
+          </div>
+          <div className={styles.master} />
+        </div>
+      </div>
+      <div className={styles.services}>
+        <div className={styles.content}>
+          <div className={styles.master} />
+          <div className={styles.righhtBlock}>
+            {priceList.slice(8, 12).map((price, index) => (
+              <div key={index}>{renderPrice(price)}</div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
